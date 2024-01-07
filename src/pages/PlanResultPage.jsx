9d715929fe98a5f9cfbbc19b7fc5742df68bf5e6
lilyVolo <lvolotchko@gmail.com>
2024-01-07 16:57:34 +0100

    prettier

--- src/pages/PlanResultPage.jsx
+++ src/pages/PlanResultPage.jsx
@@ -1,71 +1,71 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-import './PlanResultPage.css'
-import service from '../service/service';
-
+import { useLocation, useNavigate } from "react-router-dom";
+import "./PlanResultPage.css";
+import service from "../service/service";
 
 function PlanResultPage() {
-    const location = useLocation();
-    const data = location.state;
-    
-    const navigate =  useNavigate()
- 
+	const location = useLocation();
+	const data = location.state;
 
-   function handleClick () {
-    service.post(`plans/savedplans`, {city: data.city, restaurants: data.restaurants,
-      places: [data.infos[0][0], data.infos[1][0]], 
-    }).then(() => {
-      navigate('/saved')
-     })
-    
-   }
+	const navigate = useNavigate();
 
-  return (
-    <div className='container-container-plan-page' >
-   <div className='result-text'>
-    <p>This is plan of your relaxing day:</p>
-  </div>
+	function handleClick() {
+		service
+			.post(`plans/savedplans`, {
+				city: data.city,
+				restaurants: data.restaurants,
+				places: [data.infos[0][0], data.infos[1][0]],
+			})
+			.then(() => {
+				navigate("/saved");
+			});
+	}
 
-  {/* <div  className='wrapper-container-plan-page'>
+	return (
+		<div className="container-container-plan-page">
+			<div className="result-text">
+				<p>This is plan of your relaxing day:</p>
+			</div>
 
-  <div className='plan-block'>
-   <img  src="place.jpg" alt="" /> 
+			<div className="wrapper-container-plan-page">
+				<div className="plan-block">
+					<img src="place.jpg" alt="" />
 
-    <p>We propose you to visit <br/> {data.infos[0][0].name} to make yor body relaxed:</p> 
-    <p> rating of the place: {data.infos[0][0].rating} </p>
-    <p>adress: {data.infos[0][0].formatted_address}</p>
-    <p>We will book it for you, and propose extra options</p>
-  </div>
-  <div  className='plan-block'>
-  <img src={`${data.infos[1][0].img}`} alt="" />
-    <p>According to your choise we recomende you <br/> {data.infos[1][0].name}</p> 
-    <p> rating of the place: {data.infos[1][0].rating} </p>
-    <p>adress: {data.infos[1][0].formatted_address}</p>
-    <p>We will organise transfer, and degustation will be prepeared acoording to our planing</p>
-  </div>
-  <div  className='plan-block'>
-  <img src='restaurant.jpg' alt="" />
-    <p>We propose you this restaurant for a perfect dinner</p> <br/>
-     <p>{data.infos[2][0].name}</p>
-     <p>adress: {data.infos[2][0].formatted_address}</p>
-     <p> rating of the place: {data.infos[2][0].rating} </p>
+					<p>
+						We propose you to visit <br /> {data.infos[0][0].name} to make yor
+						body relaxed:
+					</p>
+					<p> rating of the place: {data.infos[0][0].rating} </p>
+					<p>address: {data.infos[0][0].formatted_address}</p>
+					<p>We will book it for you, and propose extra options</p>
+				</div>
+				<div className="plan-block">
+					<img src={`${data.infos[1][0].img}`} alt="" />
+					<p>
+						According to your choice we recommended you <br />{" "}
+						{data.infos[1][0].name}
+					</p>
+					<p> rating of the place: {data.infos[1][0].rating} </p>
+					<p>address: {data.infos[1][0].formatted_address}</p>
+					<p>
+						We will organize transfer, and dégustation will be prepared
+						according to our planing
+					</p>
+				</div>
+				<div className="plan-block">
+					<img src="restaurant.jpg" alt="" />
+					<p>We propose you this restaurant for a perfect dinner</p> <br />
+					<p>{data.infos[2][0].name}</p>
+					<p>address: {data.infos[2][0].formatted_address}</p>
+					<p> rating of the place: {data.infos[2][0].rating} </p>
+					<p>Dinner will be served immediately after your arrival</p>
+				</div>
+			</div>
 
-   
-    <p>Dinner will be served immediately after your arrival</p>
-  </div> 
-  </div> */}
-
-  
-  <button className='button-result' onClick={handleClick}> 
-    save your plan
-  </button> 
-
-  
-  <>
-  
-  </>
-    </div>
-    
-  )
+			<button className="button-result" onClick={handleClick}>
+				save your plan
+			</button>
+		</div>
+	);
 }
 
-export default PlanResultPage
+export default PlanResultPage;
